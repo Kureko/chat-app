fb5e597cf3635ef2499cf137feb38f1fe80c121f
Ugo Angeletti <ugo.angeletti@yahoo.fr>
2019-11-25 17:31:58 +0100

    Mise à jour de l'application via la quête React 5

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import './Contact.css';
 
-function Contact(props) {
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: false,
+        };
+    }
+    cliquage = () => {
+        this.setState({online: !this.state.online})
+    }
+    render() {
         return (
-            <div className="Contact">
-                    <img className="avatar" src={props.avatar}/>
-                <div>
-                    <div className="name">{props.name}</div>
-                    <div className="status">
-                        <span className={props.online ? "status-online" : "status-offline"}></span>
-                        <p className="status-text">{props.online ? "Online" : "Offline"}</p>
-                    </div>
-                </div>
-            </div>
-        );
+        <div className="Contact">
+            <img className="avatar" src={this.props.avatar}/>
+        
+        <div className="name">{this.props.name}</div>
+          <div className="status">
+            <p>{this.state.online}</p>              
+                <span 
+                className={this.state.online ? "status-online" : "status-offline"}
+                onClick={this.cliquage}
+                >                    
+                </span>
+                <p className="status-text">{this.state.online ? "Online" : "Offline"}</p>
+          </div>
+        </div>
+        )
+    }
 }
 
-
 export default Contact;
-
-
